fe15f6b095cc11d20912619f0016d66e5f358c84
Adam Suczewski <asuczewski@gmail.com>
2012-09-20 05:18:14 +0000

    new homepage stuff

---
+++ tigerapps/static/index/js/main.js
@@ -0,0 +1,35 @@
+tigerapps = {
+
+    initializeTigerapps: function() {
+        console.log("up and running");
+        setInterval(tigerapps.cycleFeaturedApp, 2000);
+
+    },
+
+    featured_app: 0,
+
+    pause_slideshow: false,
+
+    cycleFeaturedApp: function() {
+        if (tigerapps.pause_slideshow == false) {
+            tigerapps.featured_app = (tigerapps.featured_app + 1) % 5;
+            tigerapps.setFeaturedApp(tigerapps.featured_app);
+        }
+    },
+ 
+    userSelectFeaturedApp: function(feature_number) {
+        tigerapps.pause_slideshow = true;
+        tigerapps.featured_app = feature_number;
+        tigerapps.setFeaturedApp(feature_number);
+        setTimeout("tigerapps.pause_slideshow = false", 4000);
+    },
+
+    setFeaturedApp: function(feature_number) { 
+        $(".app_preview").each(function () {
+            $(this).css("background-color", "yellow");
+        });
+        $("#app_preview_" + feature_number.toString()).css("background-color", "green"); 
+        $("#photo_row").animate({"left": (feature_number*-600).toString() + "px"}, 1000); 
+        
+    }
+};
