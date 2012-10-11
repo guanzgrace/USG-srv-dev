c1db135453082f997780e878846934121d3de422
Darshan Desai <DarshanDesai17@gmail.com>
2012-10-11 02:40:18 +0000

    changed admin.py app names

--- tigerapps/www/admin.py
+++ tigerapps/www/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from models import Apps
-admin.site.register(Apps)
+from models import App
+admin.site.register(App)
 
-from models import Categories
-admin.site.register(Categories)
+from models import Category
+admin.site.register(Category)
 
 from models import Slideshow
 admin.site.register(Slideshow)
