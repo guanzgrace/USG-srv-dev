9717f7b4c2a691466249493f888517f38f76a8ac
thomasgilgenast <thomasgilgenast@gmail.com>
2012-10-17 08:29:36 +0000

    removed auto-generation of fields that no longer exist

--- tigerapps/www/admin.py
+++ tigerapps/www/admin.py
@@ -5,10 +5,6 @@ from adminsortable.admin import SortableAdmin
 class CategoryAdmin(SortableAdmin):
     list_display = ('name',)
     
-    def save_model(self, request, obj, form, change):
-        obj.index = Category.objects.all().count()
-        obj.save()
-
 admin.site.register(Category, CategoryAdmin)
 
 class AppAdmin(SortableAdmin):
@@ -16,9 +12,5 @@ class AppAdmin(SortableAdmin):
     list_editable = ('slideshow_index',)
     list_filter = ('category',)
     ordering = ['category__order', 'order']
-
-    def save_model(self, request, obj, form, change):
-        obj.category_index = App.objects.filter(category=obj.category).count()
-        obj.save()
 
 admin.site.register(App, AppAdmin)
