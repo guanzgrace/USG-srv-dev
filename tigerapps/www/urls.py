0fcace0578230d95de57d71c3440d2792d37895b
Josh Chen <joshchen@princeton.edu>
2012-11-10 19:55:52 +0000

    start of grid design at HackPrincetoTo return to stuff before, revert to commit before this one

--- tigerapps/www/urls.py
+++ tigerapps/www/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover()
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^suggestions/?', direct_to_template, {'template': 'index/suggestions.html'}),
+    url(r'^suggestions/?', direct_to_template, {'template': 'www/suggestions.html'}),
     url(r'^/?', direct_to_template, {'template': 'www/index.html'}),
 
     (r'^admin/', include(admin.site.urls)),
