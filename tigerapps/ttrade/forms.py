a281acc2908ce7dad6ed79a61f005d4a1c66aa1e
Josh Chen <joshchen@princeton.edu>
2012-11-11 22:11:04 +0000

    ttrade hacky fix of can't create listings

--- tigerapps/ttrade/forms.py
+++ tigerapps/ttrade/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from ttrade.models import *
-        
+
+class CreateForm(forms.ModelForm):
+    class Meta:
+        model=Listing
+        fields = ('method', 'description', 'picture', 'price')
+
 class ListingForm(forms.ModelForm):
     class Meta:
         model=Listing
