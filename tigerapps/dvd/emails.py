64bf9b069576a3658937856aa2612b808833ec4c
Adam Suczewski <asuczewski@gmail.com>
2012-12-01 02:33:04 +0000

    late emails

--- tigerapps/dvd/emails.py
+++ tigerapps/dvd/emails.py
@@ -25,3 +25,23 @@ def email_if_available(dvd):
         except BadHeaderError:
             send_mail("USG DVD email error", "Notification to " + notice.netid +  " has failed.", "DO_NOT_REPLY@tigerapps.org", ['AS1193@aol.com'], fail_silently=False)
             return
+
+def notify_if_late():
+    if True:#settings.REMINDER_EMAILS:
+        now = datetime.datetime.now()
+        rentalList = Rental.objects.filter(dateReturned=None).filter(dateDue__lte=now)
+        
+        subject = "[USG DVD] Reminder"
+        from_email = "DO_NOT_REPLY@tigerapps.org"
+
+        for rental in rentalList:
+            message = "Your rental of " + rental.dvd.name + " was due on " + str(rental.dateDue) + ". Please return it as soon as you can.\nThanks,\nThe USG"
+            to_email  = [rental.netid + "@princeton.edu"]
+            try:
+                send_mail(subject, message, from_email, ["AS1193@aol.com"], fail_silently=False)
+                #send_mail(subject, message, from_email, to_email)
+            except BadHeaderError:
+                send_mail("USG DVD email error", "Notification to " + to_email +  " has failed.", "DO_NOT_REPLY@tigerapps.org", ['AS1193@aol.com'], fail_silently=False)
+            
+
+
