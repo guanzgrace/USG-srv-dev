3f5089ad5cdabcd41e05acade243a56e5a44024b
Josh Giles <jgiles@princeton.edu>
2013-04-01 23:12:20 +0000

    Rooms: Restructure queue updates. Not tested.

--- tigerapps/rooms/real_time_queue.py
+++ tigerapps/rooms/real_time_queue.py
@@ -1,5 +1,5 @@
 import time
-from rooms.models import *
+from rooms.models import Room, Draw, Queue, QueueToRoom, User
 from gevent import spawn, sleep
 from gevent.event import Event
 from gevent.coros import BoundedSemaphore
@@ -10,13 +10,10 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 DEFAULT_CLEAR_INTERVAL = 10*60
 
-class LastQueueUpdate(object):
-    def __init__(self, queue_id=0, update=None):
+class QueueUpdate(object):
+    def __init__(self, queue_id):
         self.event = Event()
-        if update:
-            self.update = update
-        else:
-            self.update = QueueUpdate.objects.filter(queue__id=queue_id).order_by('-id')[0]
+        self.queue = Queue.objects.get(queue_id)
 
 class QueueManager(object):
     
@@ -29,17 +26,12 @@ class QueueManager(object):
     def _load(self, queue_id):
         """Load and return queue update tracker for queue_id."""
         self.__updates_lock.acquire()
+        # Hit.
         if queue_id in self.__updates:
             self.__updates_lock.release()
             return self.__updates[queue_id]
-        try:
-            self.__updates[queue_id] = LastQueueUpdate(queue_id=queue_id)
-        except:
-            update = QueueUpdate(queue=Queue.objects.get(id=queue_id),
-                                 timestamp = int(time.time()),
-                                 kind = QueueUpdate.EDIT,
-                                 kind_id = 1)
-            self.__updates[queue_id] = LastQueueUpdate(update=update)        
+        # Miss.
+        self.__updates[queue_id] = QueueUpdate(queue_id)
         self.__updates_lock.release()
         return self.__updates[queue_id]
 
@@ -61,8 +53,8 @@ class QueueManager(object):
             sleep(interval)
             self._clear()
 
-    def edit(self, user, queue, room_idlist, draw):
-        # Perform the work
+    def edit(self, user, queue_id, room_idlist, draw):
+        # Put together the list of Room objects.
         rooms = []
         print 'edit', room_idlist
         for roomid in room_idlist:
@@ -70,56 +62,60 @@ class QueueManager(object):
             if (not room) or not draw in room.building.draw.all():
                 return {'error':'bad room/draw'}
             rooms.append(room)
-        # Clear out the old list
+        
+        update = self._load(queue_id)
+        # Clear out the old list.
+        queue = update.queue
         queue.queuetoroom_set.all().delete()
         # Put in new relationships
         for i in range(0, len(rooms)):
             qtr = QueueToRoom(queue=queue, room=rooms[i], ranking=i)
             qtr.save()
-        
-        update = QueueUpdate(queue=queue, timestamp=int(time.time()), 
-                              kind=QueueUpdate.EDIT, kind_id=user.id)
-        update.save()
-        latest = self._load(queue.id)
-        latest.update = update
-        latest.event.set()
-        latest.event.clear()
+        # Store the update information on the queue.
+        queue.version += 1
+        queue.update_kind = Queue.EDIT
+        queue.update_user = user
+        queue.save()
+        # Notify others of the update.
+        update.event.set()
+        update.event.clear()
+        # Assemble and return response.
         room_list = []
         for room in rooms:
             room_list.append({'id':room.id, 'number':room.number,
                               'building':room.building.name})
         return {'rooms':room_list}
 
-    def check(self, user, queue, last_id):
-        print user, queue, last_id
-        latest = self._load(queue.id)
-        if last_id == latest.update.id:
+    def check(self, user, queue_id, last_version):
+        update = self._load(queue.id)
+        print user, update.queue, last_version
+        if last_version == update.queue.version:
             print 'going to wait'
-            print latest.update.id
-            latest.event.wait()
-            latest = self._load(queue.id)
+            print update.queue.version
+            update.event.wait()
+            update = self._load(queue_id)
         print 'past wait'
-        queueToRooms = QueueToRoom.objects.filter(queue=queue).order_by('ranking')
+        queueToRooms = QueueToRoom.objects.filter(queue=update.queue).order_by('ranking')
         if not queueToRooms:
-            return {'id':latest.update.id, 'rooms':[]}
+            return {'id':update.queue.version, 'rooms':[]}
         room_list = []
-        if latest.update.kind == QueueUpdate.EDIT:
-            netid = User.objects.get(pk=latest.update.kind_id).netid
+        if update.queue.update_user:
+            netid = update.queue.update_user.netid
         else:
             netid = ''
         for qtr in queueToRooms:
             room_list.append({'id':qtr.room.id, 'number':qtr.room.number,
                               'building':qtr.room.building.name})
-        return {'id':latest.update.id,
-                'kind':QueueUpdate.UPDATE_KINDS[latest.update.kind][1],
+        return {'id':update.queue.version,
+                'kind':Queue.UPDATE_KINDS[update.queue.update_kind][1],
                 'netid':netid,
                 'rooms':room_list}
 
 manager = QueueManager()
 
-def check(user, queue, last_id):
-    response = manager.check(user, queue, last_id)
-    while response.get('netid') == user.netid and last_id != 0:
+def check(user, queue, last_version):
+    response = manager.check(user, queue, last_version)
+    while response.get('netid') == user.netid and last_version != 0:
         response = manager.check(user, queue, response['id'])
     # Add in queue invitation notification.
     response['invites'] = QueueInvite.objects.filter(receiver=user).count()
