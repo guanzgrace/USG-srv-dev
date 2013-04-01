f1e23de24f3d3b1ebbb094de7e3d89ec1e60cc45
Jeremy Cohen <jmcohen27@gmail.com>
2013-04-01 20:50:05 +0000

    various UI changes

--- tigerapps/writetime/emails.py
+++ tigerapps/writetime/emails.py
@@ -12,7 +12,7 @@ def sendEmail(appointment, toName, toEmail):
 		<p>The WriteTime Team</p>
 	""" % (appointment.name, toName, toEmail, appointment.time.strftime("%I:%M %p on %A, %B %d"))
 	
-	msg = EmailMessage(subject, body, sender, [toEmail])
+	msg = EmailMessage(subject, body, sender, [appointment.netid + "@princeton.edu"])
 	msg.content_subtype = 'html'
 	msg.send()
 	
@@ -27,5 +27,21 @@ def sendEmail(appointment, toName, toEmail):
 	""" % (toName, appointment.name, appointment.email, appointment.time.strftime("%I:%M %p on %A, %B %d"))
 	
 	msg = EmailMessage(subject, body, sender, [toEmail])
+	msg.content_subtype = 'html'
+	msg.send()
+	
+def sendConfirmEmail(appointment):
+	sender = 'Write Time<princetonsectionswap@gmail.com>'
+	subject = "Your Writing Center Appointment Has Been Posted"
+	body = """
+		<p>Hello %s,</p>
+		<p>Your Writing Center appointment has been posted.  You will receive an
+		email if someone else claims it.  If you change your mind and decide to
+		keep the appointment, please remove it from the web site.</p>
+		<p>Best,</p>
+		<p>The WriteTime Team</p>
+	""" % (appointment.name)
+	
+	msg = EmailMessage(subject, body, sender, [appointment.netid + "@princeton.edu"])
 	msg.content_subtype = 'html'
 	msg.send()
