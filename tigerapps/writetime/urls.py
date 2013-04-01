305617eebc774ccf128af55a15e0d78acc1fea67
Jeremy Cohen <jmcohen27@gmail.com>
2013-04-01 14:33:05 +0000

    Copied WriteTime files over.

--- tigerapps/writetime/urls.py
+++ tigerapps/writetime/urls.py
@@ -1,21 +1,19 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls.defaults import *
+import views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'pounce.views.index', name='index'),
-    url(r'^courses$', 'pounce.views.courses', name='courses'),
-    url(r'^subscribeEmail$', 'pounce.views.subscribeEmail', name='subscribeEmail'),
-    url(r'^subscribeText$', 'pounce.views.subscribeText', name='subscribeText'),
-    url(r'^reactivate/(\d+)$', 'pounce.views.reactivate', name='reactivate'),
-#     url(r'^course-offerings/course_details.xml', 'pounce.views.coursedetails', name='coursedetails'),
-    # url(r'^princetonpounce/', include('princetonpounce.foo.urls')),
+    url(r'^$', 'writetime.views.index'),
+    url(r'^offer$', 'writetime.views.offer'),
+    url(r'^accept/(\d*)$', 'writetime.views.accept'),
+    url(r'^submitOffer/$', 'writetime.views.submitOffer'),
+    url(r'^submitAccept/(\d*)$', 'writetime.views.submitAccept'),
+    url(r'^submitRemove/(\d*)$', 'writetime.views.submitRemove'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
