b315052f8285af54a34f123eec8cb62b03c8479f
Jeremy Cohen <jmcohen27@gmail.com>
2013-04-10 05:28:50 +0000

    added HTML emails

--- tigerapps/writetime/emails.py
+++ tigerapps/writetime/emails.py
@@ -9,7 +9,7 @@ def sendMail(to, subject, body):
 	smtpserver.starttls()
 	smtpserver.ehlo
 	smtpserver.login(gmail_user, gmail_pwd)
-	header = 'To:' + to + '\n' + 'From: ' + fromEmail + '\n' + 'Subject:testing \n'
+	header = 'To:' + to + '\n' + 'From: ' + fromEmail + '\n' + 'Subject:' + subject + '\n' + 'Content-type: text/html' + '\n'
 	msg = header + '\n' + body
 	smtpserver.sendmail(gmail_user, to, msg)
 	smtpserver.close()
