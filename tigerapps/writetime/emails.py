c67fee4028078b64de0921e6f4dc28c0b5cb3fc4
Jeremy Cohen <jmcohen27@gmail.com>
2013-04-10 05:17:29 +0000

    now it should run

--- tigerapps/writetime/emails.py
+++ tigerapps/writetime/emails.py
@@ -3,13 +3,13 @@ import smtplib
 def sendMail(to, subject, body):
 	gmail_user = 'princetonwritetime@gmail.com'
 	gmail_pwd = 'dondero217'
-	from = "Write Time<princetonwritetime@gmail.com>" 
+	fromEmail = "Write Time<princetonwritetime@gmail.com>" 
 	smtpserver = smtplib.SMTP("smtp.gmail.com",587)
 	smtpserver.ehlo()
 	smtpserver.starttls()
 	smtpserver.ehlo
 	smtpserver.login(gmail_user, gmail_pwd)
-	header = 'To:' + to + '\n' + 'From: ' + from + '\n' + 'Subject:testing \n'
+	header = 'To:' + to + '\n' + 'From: ' + fromEmail + '\n' + 'Subject:testing \n'
 	msg = header + '\n' + body
 	smtpserver.sendmail(gmail_user, to, msg)
 	smtpserver.close()
