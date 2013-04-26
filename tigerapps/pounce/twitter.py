f47c3e43859e7862f318853b7564c11fdef5ea78
Jeremy Cohen <jmcohen27@gmail.com>
2013-04-26 17:30:59 +0000

    Twitter integration

--- tigerapps/pounce/twitter.py
+++ tigerapps/pounce/twitter.py
@@ -5,8 +5,8 @@ import tweepy
 consumerKey = "fMqSK9WSlqb0dpDiuijSg"
 consumerSecret = "rrE5Z8z1BfXyJofAJuYvI4Tl4bIvIf0pkrV5FlPVg"
 
-accessToken = "1213779152-RexnbcIOADAD5v6iVtl9DLSMtoLyaRlDcjJpxnO"
-accessTokenSecret = "leTvvIf9FIGWJDVwhgFfPqoRrPHRuBONF0pwEGc6RY"
+accessToken = "1213779152-bzwOZQUrOd0vREdYDEEAbf9TUz0EbqhFzgBaHGP"
+accessTokenSecret = "eWdiRW91lK3DqirM4K7ITddASUmtE8tbYoMHCz08"
 
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
