8426037d89116f2e6bc6aed3c2af40f5edeccf27
Jeremy Cohen <jmcohen27@gmail.com>
2013-04-26 19:23:32 +0000

    Fall 2013 Pounce

--- tigerapps/pounce/emails.py
+++ tigerapps/pounce/emails.py
@@ -2,4 +2,5 @@ import urllib2
 
 def sendMail(to, subject, body):
 	url = "http://princetonpounce.com/email?" + "to=" + to + "&subject=" + subject + "&body=" + body
-	return urllib2.urlopen(url).read()
+	return url
+#	return urllib2.urlopen(url).read()
