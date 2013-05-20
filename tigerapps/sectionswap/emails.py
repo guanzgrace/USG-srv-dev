84800bb6cac35aea5cfddf55f3c20d04bd5b5635
Jeremy Cohen <jmcohen27@gmail.com>
2013-05-20 05:56:38 +0000

    added sectionswap app

---
+++ tigerapps/sectionswap/emails.py
@@ -0,0 +1,7 @@
+import urllib2
+import urllib
+
+def sendMail(to, subject, body):
+	params = {'to' : to, 'subject' : subject, 'body' : body}
+	url = "http://princetonpounce.com/email?" + urllib.urlencode(params)
+	return urllib2.urlopen(url).read()
