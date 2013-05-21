47d73f220c2b7339fcd7aec5836739e2cf541cc6
Jeremy Cohen <jmcohen27@gmail.com>
2013-05-21 06:50:51 +0000

    tweaking email code

--- tigerapps/pounce/emails.py
+++ tigerapps/pounce/emails.py
@@ -1,7 +1,19 @@
 import urllib2
 import urllib
+import smtplib  
 
 def sendMail(to, subject, body):
 	params = {'to' : to, 'subject' : subject, 'body' : body}
 	url = "http://princetonpounce.com/email?" + urllib.urlencode(params)
 	return urllib2.urlopen(url).read()
+
+def sendEmail(to, subject, body):
+	passwordFile = open('EMAIL_PASSWORD')
+	password = passwordFile.read().strip()
+	passwordFile.close()
+
+	server = smtplib.SMTP('smtp.gmail.com:587')  
+	server.starttls()  
+	server.login('princetonpounce',password)  
+	server.sendmail('Princeton Pounce<princetonpounce@gmail.com', to, body)  
+	server.quit()  
