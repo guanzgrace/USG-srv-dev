393b450034f88931b3313fed802760a0390066d1
Jeremy Cohen <jmcohen27@gmail.com>
2013-05-22 02:39:19 +0000

    making sectionswap work

--- tigerapps/sectionswap/models.py
+++ tigerapps/sectionswap/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 class Course(models.Model):
     code = models.CharField(max_length=50)
