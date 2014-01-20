a7d515477569b19d508042095d3c3387e58a8d87
Jeremy Cohen <jmcohen27@gmail.com>
2014-01-20 19:57:56 +0000

    fixing pounce

--- tigerapps/pounce/emails.py
+++ tigerapps/pounce/emails.py
@@ -3,5 +3,6 @@ import urllib
 
 def sendMail(to, subject, body):
 	params = {'to' : to, 'subject' : subject, 'body' : body}
-	url = "http://princetonpounce.com/email?" + urllib.urlencode(params)
+	url = "http://jeremycohensoftware.com/email?" + urllib.urlencode(params)
 	return urllib2.urlopen(url).read()
+    
--- tigerapps/sectionswap/EMAIL_SETTINGS.py
+++
@@ -1,6 +0,0 @@
-#username = "princetonsectionswap"
-#password = "ufytzbdhqwyimiif"
-#sender = 'Section Swap<princetonsectionswap@gmail.com>'
-username = "jmcohen@princeton.edu"
-password = "Ptonis4me!"
-sender = "Section Swap<jmcohen@princeton.edu>"
