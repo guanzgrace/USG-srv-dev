9e1c8c2e89d446679b53dfcd00ba5d027e9a637b
bnprks <bnprks@gmail.com>
2014-04-29 22:44:06 -0400

    Fixed up the email system

--- tigerapps/pounce/emails.py
+++ tigerapps/pounce/emails.py
@@ -1,8 +1,17 @@
-import urllib2
-import urllib
-
+import os
+import smtplib
+from email.mime.text import MIMEText
 def sendMail(to, subject, body):
-	params = {'to' : to, 'subject' : subject, 'body' : body}
-	url = "http://jeremycohensoftware.com/email?" + urllib.urlencode(params)
-	return urllib2.urlopen(url).read()
-    
+    smtp = smtplib.SMTP_SSL("smtp.princeton.edu")
+    # POUNCE_EMAIL_USER and POUNCE_EMAIL_PASS should be environment
+    # variables containing the netid and the password for the email sender
+    user = os.environ["POUNCE_EMAIL_USER"]
+    password = os.environ["POUNCE_EMAIL_PASS"] 
+    smtp.login(user, password)
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = "Princeton Pounce"
+    msg['To'] = to
+    response = smtp.sendmail(user + "@princeton.edu", [to], msg.as_string())
+    smtp.quit() 
+    return response
