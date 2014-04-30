5511c6fccc9d9e2b3f90934046ef3849c782a989
bnprks <bnprks@gmail.com>
2014-04-29 23:02:01 -0400

    Changed to import login info from a file rather than environment variables

--- tigerapps/pounce/emails.py
+++ tigerapps/pounce/emails.py
@@ -1,12 +1,9 @@
 import os
 import smtplib
 from email.mime.text import MIMEText
+from EMAIL_SETTINGS import user, password 
 def sendMail(to, subject, body):
     smtp = smtplib.SMTP_SSL("smtp.princeton.edu")
-    # POUNCE_EMAIL_USER and POUNCE_EMAIL_PASS should be environment
-    # variables containing the netid and the password for the email sender
-    user = os.environ["POUNCE_EMAIL_USER"]
-    password = os.environ["POUNCE_EMAIL_PASS"] 
     smtp.login(user, password)
     msg = MIMEText(body)
     msg['Subject'] = subject
